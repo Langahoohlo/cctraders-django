ad40f5e0e0e76cbe0121c462d02018c293586600
langahoohlo <langahoohlo@gmail.com>
2024-02-08 18:15:00 +0200

    more django additions to the website

--- cctraders/index/urls.py
+++ cctraders/index/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import index_view
+from .views import index_view, farmer_view, logistics_view, waste_management_view
 
 urlpatterns = [
-    path('', index_view, name='index'),
+    path('index.html', index_view, name='index'),
+    path('farmer-page.html', farmer_view, name='farmer-page'),
+    path('logistics-page.html', logistics_view, name='logistics-page'),
+    path('waste-management.html', waste_management_view, name='waste-management'),
     # path('contact/', ContactFormView.as_view(), name='contact-form'),
 ]
--- cctraders/index/views.py
+++ cctraders/index/views.py
@@ -7,6 +7,14 @@ def index_view(request):
     service_one = ServiceOne.objects.all()
     {'service_ones': service_one}
 
+    # Check if there is at least one object in the queryset
+    if service_one.exists():
+        # Pass only the first object to the context
+        context = {'service_one': service_one[0]}
+    else:
+        # Handle the case when there are no objects
+        context = {'service_one': None}
+
     service_two = ServiceTwo.objects.all()
     {'service_two': service_two}
 
@@ -14,3 +22,15 @@ def index_view(request):
     {'service_three': service_three}
 
     return render(request, 'index.html', {'service_ones': service_one})
+
+def farmer_view(request):
+
+    return render(request, 'farmer-page.html')
+
+def logistics_view(request):
+
+    return render(request, 'logistics-page.html')
+
+def waste_management_view(request):
+
+    return render(request, 'waste-management.html')
